4cddf6b7314088db6a15e71722f7bac2cb4e0c8e
Awenk <33746071+badueny@users.noreply.github.com>
2026-05-13 13:05:44 +0700

    Export both S3Driver and LocalDriver

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 const S3Driver = require('./drivers/S3Driver');
+const LocalDriver = require('./drivers/LocalDriver');
 
-module.exports = S3Driver;
+module.exports = {
+    S3Driver,
+    LocalDriver
+};
