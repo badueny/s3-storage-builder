2c0a2e5f94c6d8341dbcafc8b3bd1fc439fd7fd4
Awenk <33746071+badueny@users.noreply.github.com>
2026-05-13 12:34:58 +0700

    Delete helpers directory

--- helpers/mime.js
+++
@@ -1,7 +0,0 @@
-const mime = require('mime-types');
-
-module.exports = function detectMime(filename) {
-
-    return mime.lookup(filename) || 'application/octet-stream';
-
-};
--- helpers/path.js
+++
@@ -1,8 +0,0 @@
-module.exports = function buildPath(...paths) {
-
-    return paths
-        .filter(Boolean)
-        .join('/')
-        .replace(/\/+/g, '/');
-
-};
